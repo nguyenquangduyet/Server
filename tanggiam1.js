1fee9dee3b166e2106f79e40208d86e654cceca9
nguyễn quang duyệt <80259210+nguyenquangduyet@users.noreply.github.com>
2025-07-01 15:35:05 +0700

    Add files via upload

---
+++ tanggiam1.js
@@ -0,0 +1,14 @@
+﻿var a = 3;
+var b =5;
+ //thực hiện phép tính 
+ // a++ * b-- - --a + b++ //a =3 , b=5
+ // 3 * b-- - --a + b++ // a= 4 , b=5
+ // 3 * 5 - --a + b++ // a=4,b =4
+ // 3 * 5 - 3 + b++ // a=3,b=4
+ // 3 * 5 -3 + 4 = 16
+ var c = a++ * b-- - --a + b++;
+ console.log(c);
+ console.log(a++);
+ console.log(b--);
+ console.log(--a);
+ console.log(b++);
